f1ea3ed0e98d7aaf71945c496db1201db3e42b60
Charles Edward Pax <charles.pax@gmail.com>
2019-03-13 12:45:19 -0400

    Added a python3 script. Trying this out.

---
+++ pynode.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import npyscreen
+
+class App(npyscreen.StandardApp):
+    def onStart(self):
+        self.addForm("MAIN", MainForm, name="Hello Medium!")
+
+class MainForm(npyscreen.ActionForm):
+    # Constructor
+    def create(self):
+        # Add TitleText sidget to the form
+        self.title = self.add(npyscreen.TitleText, name="TitleText", value="Hello World!")
+    # Override method that triggers when you click "ok"
+    def on_ok(self):
+        self.parentApp.setNextForm(None)
+    # Override method that triggers when you click "cancel"
+    def on_cancel(self):
+        self.title.value = "Hello World!"
+
+MyApp = App()
+MyApp.run()
