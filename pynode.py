a17e30c8c02ee554b9bea21fd46077f350453a1c
Charles Edward Pax <charles.pax@gmail.com>
2019-03-13 14:07:53 -0400

    Added error ahndling to npyscreen code to work around a bug.

--- pynode.py
+++ pynode.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import npyscreen
 
 class App(npyscreen.StandardApp):
@@ -19,4 +20,10 @@ class MainForm(npyscreen.ActionForm):
         self.title.value = "Hello World!"
 
 MyApp = App()
-MyApp.run()
+# npyscreen has a bug in Python 3.7. The workaround is to catch the
+# StopIteration errors. Otherwise, this script will exit.
+try:
+    MyApp.run()
+except StopIteration:
+    print("ERROR")
+
